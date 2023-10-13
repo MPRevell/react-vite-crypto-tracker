a89dafc6788a630a3002620491c36f669cd99089
Samir HABIB ZAHMANI <Unforgiven-wanda@hotmail.fr>
2023-10-13 15:15:37 +0100

    HyperUI components examples

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import HyperUIComponentExample from "../components/HyperUIComponentExample";
+import HyperUIComponentExample2 from "../components/HyperUIComponentExample2";
 
 const About = () => {
-  return <div>This is the about page. See AboutPage.jsx</div>;
+  return (
+    <div>
+      This is the about page. See AboutPage.jsx
+      <div style={{ width: "50%", margin: "0 auto", marginTop: "1em" }}>
+        HyperUI Component Examples:
+        <HyperUIComponentExample />
+        <HyperUIComponentExample2 />
+      </div>
+    </div>
+  );
 };
 
 export default About;
