2a79d7848bd185b2db6042cbcc62a85804d71ad2
Matt <mprevell97@gmail.com>
2023-10-17 21:24:00 +0100

    added mock data for use in chartJs

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,51 +1,10 @@
-import Banner from "../components/Banner";
-import Footer from "../components/shared/Footer";
-import TestTable from "../components/TestTable";
-import Navbar from "../components/shared/Navbar";
-import {
-  Typography,
-  Container,
-  Toolbar,
-  AppBar,
-  Select,
-  MenuItem,
-  makeStyles,
-  styled,
-  createTheme,
-  ThemeProvider,
-} from "@mui/material";
+import chartData from "../data/chartData";
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
 
+const sparklineData = chartData.coin.sparkline;
 export default function TestPage() {
-  const TitleTypography = styled(Typography)({
-    flex: 1,
-  });
+  return <div><Line data={sparklineData}
 
-  return (
-    <div>
-      <AppBar color="transparent" position="static">
-        <Container>
-          <Toolbar>
-            <TitleTypography className="brand-name">
-              Matt's CryptoVerse
-            </TitleTypography>
-            <Select
-              value={""}
-              className="text-sky-400"
-              variant="outlined"
-              style={{
-                width: 100,
-                height: 40,
-                marginLeft: 15,
-              }}
-            >
-              <MenuItem value={"USD"}>USD</MenuItem>
-              <MenuItem value={"GBP"}>GBP</MenuItem>
-              <MenuItem value={"EUR"}>EUR</MenuItem>
-            </Select>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Banner />
-    </div>
-  );
+  ></div>;
 }
