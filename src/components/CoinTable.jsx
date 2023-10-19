30b31281bc680d41469e32ba681e610b675b5de0
Matt <mprevell97@gmail.com>
2023-10-19 16:53:58 +0100

    dynamic chart, for each crypto will now show 7d price action and looks good - however the chart needs formatting to fit with rest of page

--- src/components/CoinTable.jsx
+++ src/components/CoinTable.jsx
@@ -9,85 +9,87 @@ const CoinTable = ({ data }) => {
 
   return (
     <>
-      <div className="overflow-x-auto">
-        <table className="w-full text-sm text-left bg-sky-500 text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-white-700  bg-gray-50 dark:bg-gray-950 dark:text-gray-200">
-            <tr>
-              <th scope="col" className="px-6 py-3 text-center">
-                #
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3 text-center">
-                24hr %
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Market Cap
-              </th>
-              <th scope="col" className="px-6 py-3 text-center">
-                7d Chart
-              </th>
-              <th scope="col" className="px-6 py-3 text-center">
-                Add to Watchlist
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((coin) => (
-              <tr
-                onClick={() => {
-                  navigate(`/about/${coin.uuid}`);
-                }}
-                key={coin.uuid}
-                className="bg-white border-b dark:bg-gray-900 dark:border-gray-700 overflow-x-auto"
-              >
-                <td className="text-center">{coin.rank}</td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  <div className="coin">
-                    {/* <img src={coin.image} alt={coin.symbol} width={20} /> */}
-                    <h4>{coin.name}</h4>
-                    <small>{coin.symbol}</small>
-                    <img src={coin.iconURL} alt="Logo" />
-                  </div>
+      <div className="overflow-x-auto px-8">
+        <div className="table-container">
+          <table className="w-full text-sm text-left bg-sky-500 text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-white-700  bg-gray-50 dark:bg-gray-950 dark:text-gray-200">
+              <tr>
+                <th scope="col" className="px-6 py-3 text-center">
+                  #
                 </th>
-                <td className="px-6 py-4">
-                  {formatNumbers(parseFloat(coin.price).toFixed(2))}
-                </td>
-                <td className="px-6 py-4 text-center">
-                  <span
-                    className={`text-xs font-medium ${
-                      parseFloat(coin.change) > 0
-                        ? "text-green-600"
-                        : "text-red-600"
-                    }`}
-                  >
-                    {parseFloat(coin.change).toFixed(2)}%
-                  </span>
-                </td>
-                <td className="px-6 py-4">
-                  {formatNumbers(parseFloat(coin.marketCap).toFixed(2))}
-                </td>
-                <td className="px-6 py-4">Coin chart</td>
-                <td className="px-6 py-4 text-center">
-                  <a
-                    href="#"
-                    className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-                    // Here I want to add functionality to click Add and is stored on user's watchlist.
-                  >
-                    Add
-                  </a>
-                </td>
+                <th scope="col" className="px-6 py-3">
+                  Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3 text-center">
+                  24hr %
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Market Cap
+                </th>
+                <th scope="col" className="px-6 py-3 text-center">
+                  7d Chart
+                </th>
+                <th scope="col" className="px-6 py-3 text-center">
+                  Add to Watchlist
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {data.map((coin) => (
+                <tr
+                  onClick={() => {
+                    navigate(`/about/${coin.uuid}`);
+                  }}
+                  key={coin.uuid}
+                  className="bg-white border-b dark:bg-gray-900 dark:border-gray-700 overflow-x-auto"
+                >
+                  <td className="text-center">{coin.rank}</td>
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    <div className="coin">
+                      {/* <img src={coin.image} alt={coin.symbol} width={20} /> */}
+                      <h4>{coin.name}</h4>
+                      <small>{coin.symbol}</small>
+                      <img src={coin.iconURL} alt="Logo" />
+                    </div>
+                  </th>
+                  <td className="px-6 py-4">
+                    {formatNumbers(parseFloat(coin.price).toFixed(2))}
+                  </td>
+                  <td className="px-6 py-4 text-center">
+                    <span
+                      className={`text-xs font-medium ${
+                        parseFloat(coin.change) > 0
+                          ? "text-green-600"
+                          : "text-red-600"
+                      }`}
+                    >
+                      {parseFloat(coin.change).toFixed(2)}%
+                    </span>
+                  </td>
+                  <td className="px-6 py-4">
+                    {formatNumbers(parseFloat(coin.marketCap).toFixed(2))}
+                  </td>
+                  <td className="px-6 py-4">Coin chart</td>
+                  <td className="px-6 py-4 text-center">
+                    <a
+                      href="#"
+                      className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+                      // Here I want to add functionality to click Add and is stored on user's watchlist.
+                    >
+                      Add
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,11 @@
 // import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Line } from "react-chartjs-2";
+import { CategoryScale, Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+Chart.register(CategoryScale);
 
 const About = () => {
   const { coin } = useParams();
@@ -20,13 +25,85 @@ const About = () => {
     requestCoinDetails();
   }, [coin]);
 
+  /*
+
+  to change time period => we use ?timePeriod=${timePeriod} // I want to set this to a button, so when users select a different time it re-renders and updates with the relevant info.
+
+  I want 24hr (default) 7d, 30d, 3m, 1y, 3y - implement the logic to change this.
+
+  Add functionality whereby if latest entry of data is higher than first entry => Green line : red line
+
+  */
+
   return (
-    <div>
+    <div className="sss">
       {data ? (
         <>
-          This is the about page.
-          <div style={{ width: "50%", margin: "0 auto", marginTop: "1em" }}>
-            <div> Lets see if data is passing correctly {data?.name} </div>
+          <div className="flex flex-col p-4 h-screen justify-center items-center text-blue-900 dark:text-slate-200 bg-gray-100 dark:bg-gray-800">
+            {data.name}: ${parseFloat(data.price).toFixed(2)}
+            <h1 className="text-4xl font-bold text-blue-500 dark:text-blue-300 mb-2">
+              {data.name}
+            </h1>
+            <p className="text-lg text-gray-600 dark:text-gray-400 mb-4">
+              {data.description}
+            </p>
+            <div className="coin-chart w-11/12 lg:w-3/4 xl:w-2/3 mb-6 bg-blue-200 dark:bg-gray-900 h-64 rounded-lg p-4">
+              <Line
+                data={{
+                  labels: [
+                    "1",
+                    "2",
+                    "3",
+                    "4",
+                    "5",
+                    "6",
+                    "7",
+                    "8",
+                    "9",
+                    "10",
+                    "11",
+                    "12",
+                    "13",
+                    "14",
+                    "15",
+                    "16",
+                    "17",
+                    "18",
+                    "19",
+                    "20",
+                    "21",
+                    "22",
+                    "23",
+                    "24",
+                  ],
+                  datasets: [
+                    {
+                      label: "Price ($)",
+                      data: data.sparkline.map((str) =>
+                        parseFloat(str).toFixed(2)
+                      ),
+                    },
+                  ],
+                }}
+              />
+            </div>
+            {/* Time Period Buttons */}
+            <div className="coin-chart-buttons flex items-center space-x-4 mt-4">
+              {" "}
+              {/* Added items-center and space-x-4 for spacing and alignment */}
+              <span className="text-lg text-gray-600 dark:text-gray-400">
+                Time Period:
+              </span>{" "}
+              {/* Label for the time periods */}
+              {["1H", "3H", "12H", "24H", "7D", "30D"].map((timePeriod) => (
+                <button
+                  key={timePeriod}
+                  className="px-4 py-2 bg-blue-500 dark:bg-blue-700 text-white rounded-md hover:bg-blue-600 dark:hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600"
+                >
+                  {timePeriod}
+                </button>
+              ))}
+            </div>
           </div>
         </>
       ) : (
